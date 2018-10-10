2200618ab6376dcbebce5b7535416895eac6051e
Claus09 <beltranclaudio09@gmail.com>
2018-10-09 22:17:18 -0700

    Examen ha sido corregido con lo que se indicó, se arreglaron partes en las capas del modelo de la red, se añadieron comentarios y se eliminaron librerías inecesarias

--- EvaluacionU2/evalU2.scala
+++ EvaluacionU2/evalU2.scala
@@ -1,23 +1,37 @@
-import org.apache.spark.mllib.linalg.{Vector, Vectors}
-import org.apache.spark.sql.Row
+
 import org.apache.spark.ml.feature.LabeledPoint
-import spark.implicits._
-import org.apache.spark.sql.types.DoubleType
 import org.apache.spark.rdd.RDD
-import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.mllib.linalg.Vectors
 
 import org.apache.spark.sql.SparkSession
 val spar = SparkSession.builder().getOrCreate()
 
 //1.- LIMPIEZA DE DATOS
-val data: RDD[String] = sc.textFile("Iris.csv")
-val parsedData: RDD[LabeledPoint] = data.map{line =>
+/*
+Primero se carga el archivo CSV en una variable val de tipo RDD[String], posteriormente en una variable val de
+tipo RDD[LabeledPoint] mapearemos lo que contiene raw con los parámetros que nostros queremos:
+Nuestro LabeledPoint tendrá como primer parámetro las clases (setosa, versicolor, virginica) que son dados por
+resultados de los case como 1.0,2.0,3.0 respectivamente y un caso default si es que no cumple con alguno de ellos.
+Utilizamos parts(0) para indicarle dentro de Vectors.dense (que es nuestro segundo parámetro de LabeledPoint) que
+queremos los elementos de la columna que se desea, en este caso necesitamos las primeras 4 colummnas y esto lo
+transformamos a vector tipo ml usando .asML
+Por último convertimos en una variable val nuestro archivi limpio como un Dataframe con las columna "label"
+y "features" que son las que ocupamos para el algoritmo de MLP.
+*/
+val raw: RDD[String] = sc.textFile("Iris.csv")
+
+val cldata: RDD[LabeledPoint] = raw.map{line =>
   val parts = line.split(',')
-  LabeledPoint(parts(0).toDouble, Vectors.dense(parts(1).toDouble,parts(2).toDouble,parts(3).toDouble).asML)
+  LabeledPoint(parts(4) match {
+    case "Iris-setosa" => 1.0
+    case "Iris-versicolor" => 2.0
+    case "Iris-virginica" => 3.0
+    case _ => 0.0
+  }, Vectors.dense(parts(0).toDouble,parts(1).toDouble,parts(2).toDouble,parts(3).toDouble).asML)
   }.cache()
 
-val alt_df = parsedData.toDF("label","features")
+
+val df = cldata.toDF("label","features")
 //---------------------------------------------------
 
 //ALGORITMO ML PERCPETRÓN CAPA MULTIPLE (MLP)
@@ -26,7 +40,7 @@ import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 
 //Se seleccionó un 60% de entrenamiento y un 40% de prueba con una semilla 1234L que hace referencia a los pesos (long)
-val splits = alt_df.randomSplit(Array(0.6, 0.4), seed = 1234L)
+val splits = df.randomSplit(Array(0.6, 0.4), seed = 1234L)
 //Guardamos nuestras sepraciones de pentrenamiento y de prueba
 val train = splits(0)
 val test = splits(1)
@@ -34,8 +48,8 @@ val test = splits(1)
 
 //b.-
 //Se especifican los nodos de nuestras capas
-// La capa de entrada será 3 (3 características), dos nodos escondidos de 4 cada uno y 150 de salida (clases)
-val layers = Array[Int](3, 4, 4, 150)
+// La capa de entrada será 4 (4 características), dos nodos escondidos de 4 cada uno y 4 de salida (clases)
+val layers = Array[Int](4, 4, 4, 4)
 //---------------------------------------------------
 
 //c.-
